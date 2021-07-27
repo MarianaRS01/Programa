26ad6db55668d040038bf5d710e76df0dda0143c
MarianaRS01 <87503414+MarianaRS01@users.noreply.github.com>
2021-07-26 22:54:34 -0500

    Triángulo
    Se obtiene la hipotenusa de un triángulo rectángulo.

---
+++ Triangulo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float hipotenusa
+    float cateto1
+    float cateto2 
+
+    printf("Digite el primer cateto a: ");
+    scanf (%f,&cateto1);
+    printf("Digite el segundo cateto b: ");
+    scanf (%f,&cateto2);
+
+    hipotenusa = sqrt(pow(cateto1,2)+pow(cateto2,2));
+    printf ("La hipotenusa del triángulo es: %f",hipotenusa);
+
+    return 0;
+}
